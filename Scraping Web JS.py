271e093773006c1aa56a3e1f677dd711acb48d74
Irsyadur <113742875+Irsyadur@users.noreply.github.com>
2022-11-16 09:47:29 +0700

    Update Scraping Web JS.py

--- Scraping Web JS.py
+++ Scraping Web JS.py
@@ -1,9 +1,5 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 import pandas as pd
 import time
 
